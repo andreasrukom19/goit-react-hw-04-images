c007f38632e319968f3f8db648e9ba98d97f2711
Андрій <rukom@ukr.net>
2024-01-07 20:13:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Searchbar } from 'components/Searchbar/Searchbar';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { onDataSearch } from 'services/api';
 import Notiflix from 'notiflix';
 import { Loader } from 'components/Loader/Loader';
@@ -7,90 +7,94 @@ import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Button } from 'components/Button/Button';
 import { Modal } from 'components/Modal/Modal';
 
-export class App extends Component {
-  state = {
-    dataImages: null,
-    status: 'idle',
-    error: null,
-    searchValue: '',
-    page: 1,
-    loading: false,
-    isOpenModal: false,
-    modalData: null
-  }
+export const App = () => {
+  const [dataImages, setDataImages] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchValue !== this.state.searchValue || prevState.page !== this.state.page) {
-      this.fetchImagesByQuery(this.state.page, this.state.searchValue)
-        .then(data => {
-          this.setState(prevState => (
-            prevState.dataImages ? {dataImages: [...prevState.dataImages, ...data.hits]} : {dataImages: data.hits}
-          ))
-        });
+  useEffect(() => {
+    if (searchValue) {
+      fetchImagesByQuery(page, searchValue).then(data => {
+        setDataImages(prevDataImages =>
+          prevDataImages ? [...prevDataImages, ...data.hits] : data.hits
+        );
+      });
     }
-  }
+  }, [searchValue, page]);
 
-  onSubmitForm = formData => {
-    if (formData.inputValue === this.state.searchValue) {
-      Notiflix.Notify.warning('You are making a similar request!', {position: 'center-top', width: '400px', fontSize: '16px'});
+  const onSubmitForm = formData => {
+    if (formData.inputValue === searchValue) {
+      Notiflix.Notify.warning('You are making a similar request!', {
+        position: 'center-top',
+        width: '400px',
+        fontSize: '16px',
+      });
       return;
     }
-    this.setState({
-      searchValue: formData.inputValue,
-      dataImages: null,
-      page: 1,
-    });
-  }
 
-  fetchImagesByQuery = async (page, searchValue) => {
+    setSearchValue(formData.inputValue);
+    setDataImages(null);
+    setPage(1);
+  };
+
+  const fetchImagesByQuery = async (page, searchValue) => {
     try {
-      this.setState({ status: 'pending' });
+      setStatus('pending');
       const requestData = await onDataSearch(page, searchValue);
-      this.setState({status: 'resolved', loading: this.state.page < Math.ceil(requestData.totalHits / 12)});
+      setStatus('resolved');
+      setLoading(page < Math.ceil(requestData.totalHits / 12));
       return requestData;
     } catch (error) {
-      this.setState({ error: error.message, status: 'rejected' });
+      setError(error.message);
+      setStatus('rejected');
     }
-  }
+  };
 
-  onLoadMore = () => {
-    this.setState({page: this.state.page + 1})
-  }
+  const onLoadMore = () => {
+    setPage(page + 1);
+  };
 
-  handleShowImage = selectId => {
-    const selectedImg = this.state.dataImages.find(image => image.id === selectId);
-    this.setState({
-      isOpenModal: true,
-      modalData: selectedImg
-    })
-  }
+  const handleShowImage = selectId => {
+    const selectedImg = dataImages.find(image => image.id === selectId);
 
-  handleCloseModal = () => {
-    this.setState({ isOpenModal: false });
-  }
+    setIsOpenModal(true);
+    setModalData(selectedImg);
+  };
 
-  render() {
-    const { status, dataImages, loading, isOpenModal, modalData, } = this.state;
-    const paramsMessage = {
-      distance: '70px',
-      fontSize: '18px',
-      width: '400px',
-      position: 'center-top'
-    };
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
 
-    return (
-      <div>
-        <Searchbar onSubmit={this.onSubmitForm} />
-        {status === 'rejected' && Notiflix.Notify.failure('Oops, the image cannot be loaded')}
-        {Array.isArray(dataImages) && dataImages.length === 0 &&
-          Notiflix.Notify.info('Nothing was found for your request', paramsMessage)}
-        <ImageGallery images={this.state.dataImages} handleShowImage={this.handleShowImage} />
-        {status === 'pending' && <Loader />}
-        {Array.isArray(dataImages) && dataImages.length > 0 && loading &&
-          <Button onLoadMore={this.onLoadMore} />}
-        {isOpenModal && <Modal modalData={ modalData } handleCloseModal={this.handleCloseModal} />}
-      </div>
-    )
-  }
-}
+  const paramsMessage = {
+    distance: '70px',
+    fontSize: '18px',
+    width: '400px',
+    position: 'center-top',
+  };
 
+  return (
+    <div>
+      <Searchbar onSubmit={onSubmitForm} />
+      {error && Notiflix.Notify.failure('Oops, the image cannot be loaded')}
+      {Array.isArray(dataImages) &&
+        dataImages.length === 0 &&
+        Notiflix.Notify.info(
+          'Nothing was found for your request',
+          paramsMessage
+        )}
+      <ImageGallery images={dataImages} handleShowImage={handleShowImage} />
+      {status === 'pending' && <Loader />}
+      {Array.isArray(dataImages) && dataImages.length > 0 && loading && (
+        <Button onLoadMore={onLoadMore} />
+      )}
+      {isOpenModal && (
+        <Modal modalData={modalData} handleCloseModal={handleCloseModal} />
+      )}
+    </div>
+  );
+};
